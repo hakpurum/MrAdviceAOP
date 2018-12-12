3b8816249c051c2977c822545b65c62fafd446e6
Hasan Akpürüm <hakpurum@gmail.com>
2018-12-12 15:57:36 +0300

    MrAdvice AOP
    MrAdvice AOP c# Aspect Oriented Programming

---
+++ AOPMrAdvice/LogingAspect.cs
@@ -0,0 +1,44 @@
+﻿using ArxOne.MrAdvice.Advice;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPMrAdvice
+{
+    public class LogingAspect : Attribute, IMethodAdvice
+    {
+        public void Advise(MethodAdviceContext context)
+        {
+            try
+            {
+                //Method ya da class a gönderilen değerler alındı
+                IList<object> arguments = context.Arguments;
+
+                Console.WriteLine(context.TargetName + " Method'a girdi");
+                if (arguments != null && arguments.Count > 0)
+                {
+                    foreach (var arg in arguments)
+                    {
+                        //değerler ekrana yazdırıldı
+                        Console.WriteLine("Parametre : " + arg);
+                    }
+                }
+
+                //Method işlemi yürütüldü
+                context.Proceed();
+
+                Console.WriteLine(context.TargetName + " Methodan çıktı");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("hata oluştu");
+                Debug.WriteLine(ex);
+            }
+        }
+
+    }
+
+}
---
+++ AOPMrAdvice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPMrAdvice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var detail = new UserService().GetUserName(1);
+            Console.Write(detail);
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public class UserService
+    {
+        [LogingAspect]
+        public string GetUserName(int id)
+        {
+            throw new ArgumentNullException();
+            return "Hasan Akpürüm";
+        }
+    }
+}
